b6164fea4f930cc60c658161b0e1fe347e763227
Waqas D <wdaimee@gmail.com>
2020-06-10 19:23:37 -0400

    created create-user.dto.ts file

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class CreateUserDto {
+    @Field() readonly id?: string;
+    @Field() readonly username: string;
+    @Field() readonly email: string;
+    @Field() readonly password: string;
+    @Field() readonly firstName: string;
+    @Field() readonly lastName: string;
+}
