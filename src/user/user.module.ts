3dfa1493d617599df8a2000e183c20476c50b476
Waqas D <wdaimee@gmail.com>
2020-06-21 19:20:35 -0400

    added dotenv to dependencies

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,3 +1,4 @@
+import { UserResolver } from './user.resolver';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
@@ -5,7 +6,7 @@ import { User } from './user.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    providers: [UserService],
+    providers: [UserResolver, UserService],
     controllers: [],
 })
 
