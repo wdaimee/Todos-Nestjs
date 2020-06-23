25ec362ac78fed620f5c74f6452713efbfe32b59
Waqas D <wdaimee@gmail.com>
2020-06-23 14:02:06 -0400

    updated auth.service.ts file to remove errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,6 +12,7 @@ export class AuthService {
     async validateUser(username: string, pass: string): Promise<any> {
         const user = await this.userService.findOne(username);
         if (user && user.password === pass) {
+            // eslint-disable-next-line
             const { password, ...result } = user;
             return result;
         }
@@ -21,7 +22,7 @@ export class AuthService {
     async login(user: any) {
         const payload = { username: user.username, sub: user.userId };
         return {
-            access_token: this.jwtService.sign(payload),
+            accessToken: this.jwtService.sign(payload),
         }
     }
 }
