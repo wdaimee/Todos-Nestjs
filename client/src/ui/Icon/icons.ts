828fb0c22e46fbb268ccdba87d61329dee84b13a
Waqas D <wdaimee@gmail.com>
2020-08-06 14:03:05 -0400

    updated history icon to react fc and added to icons.tsx

--- client/src/ui/Icon/icons.ts
+++ client/src/ui/Icon/icons.ts
@@ -1,7 +1,9 @@
 import { Add } from './Icons/add-24px';
 import { Home } from './Icons/home-24px';
+import { History } from './Icons/history-24px';
 
 export const icons = {
     addIcon: Add,
-    homeIcon: Home
+    homeIcon: Home,
+    historyIcon: History
 };
