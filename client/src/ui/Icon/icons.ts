3c031e1619296f8a1650ec0ef629c46de506299e
Waqas D <wdaimee@gmail.com>
2020-08-12 11:15:19 -0400

    added clear icon tsx and to icons list

--- client/src/ui/Icon/icons.ts
+++ client/src/ui/Icon/icons.ts
@@ -2,10 +2,12 @@ import { Add } from './Icons/add-24px';
 import { Home } from './Icons/home-24px';
 import { History } from './Icons/history-24px';
 import { Profile } from './Icons/profile';
+import { Clear } from './Icons/clear';
 
 export const icons = {
     addIcon: Add,
     homeIcon: Home,
     historyIcon: History,
-    profileIcon: Profile
+    profileIcon: Profile,
+    clearIcon: Clear
 };
