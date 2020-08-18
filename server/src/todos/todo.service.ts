47721f8ce614138de78af5413e40a80483a1ef00
Waqas D <wdaimee@gmail.com>
2020-08-18 19:22:19 -0400

    updated todos query to get all todos for today and future

--- server/src/todos/todo.service.ts
+++ server/src/todos/todo.service.ts
@@ -16,12 +16,13 @@ export class TodoService {
 
     // Find all Todos that belong to a user
     findAllTodos(user: User): Promise<Todo[]> {
-        return this.todoRepository.find({ where: { user }, order: { dueDate: "ASC" } });
+        return this.todoRepository.find({ where: { user }, order: { dueDate: "ASC", status: "DESC" } });
     }
 
     // Find all Todos for today and future
     findAllTodosToday(user: User): Promise<Todo[]> {
-        return this.todoRepository.find({ where: { user, dueDate: MoreThanOrEqual(new Date()) }, order: { dueDate: "ASC" } });
+        let today = new Date().setHours(0,0,0,0)
+        return this.todoRepository.find({ where: { user, dueDate: MoreThanOrEqual(new Date(today)) }, order: { dueDate: "ASC", status: "DESC" } });
     }
 
     // Get all Todos for a user that are currently open
