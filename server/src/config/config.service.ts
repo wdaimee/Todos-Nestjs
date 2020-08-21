75049be2586a0023110e129f0b18aa5b5718642b
Waqas D <wdaimee@gmail.com>
2020-08-21 10:35:52 -0400

    changed entities to dist in config.service for production

--- server/src/config/config.service.ts
+++ server/src/config/config.service.ts
@@ -38,7 +38,7 @@ class ConfigService {
             password: this.getValue('POSTGRES_PASSWORD'),
             database: this.getValue('POSTGRES_DATABASE'),
 
-            entities: ['**/*.entity{.ts,.js}'],
+            entities: ['dist/**/*.entity{.ts,.js}'],
 
             migrationsTableName: 'migration',
 
