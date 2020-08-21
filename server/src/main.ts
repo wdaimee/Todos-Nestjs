7b8000cf8554a3c963c98912456a9362b7060c90
Waqas D <wdaimee@gmail.com>
2020-08-21 18:09:22 -0400

    updated main.ts file to include serving static files

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,11 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(join(process.cwd(), '../client/build')));
+  }
+
   await app.listen(process.env.PORT || 3001);
 }
 
